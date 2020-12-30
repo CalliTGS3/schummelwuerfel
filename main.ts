e121ee6e154ac4dfc924be83e05d56b4f5b46984
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-12-30 15:15:29 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -62,28 +62,28 @@ input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
     }
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showIcon(IconNames.Chessboard)
-    basic.pause(100)
-    basic.clearScreen()
-    Intervall = 2 ** Austrudeln
-    for (let index = 0; index < Austrudeln; index++) {
-        Intervall = Intervall / 2
-        if (Intervall == 1) {
-            Augen = randint(MinimumAugen, 6)
-            basic.setLedColor(0x00ff00)
-            basic.pause(100)
-            basic.turnRgbLedOff()
-        } else {
-            Augen = randint(1, 6)
-        }
-        ZeigeAugen(Augen)
-        basic.pause(Intervall)
-    }
-})
 let Augen = 0
 let Intervall = 0
 let MinimumAugen = 0
-let Austrudeln = 0
-Austrudeln = 10
+let Austrudeln = 10
 MinimumAugen = 1
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.turnRgbLedOff()
+        basic.showIcon(IconNames.Chessboard)
+        basic.pause(100)
+        basic.clearScreen()
+        Intervall = 2 ** Austrudeln
+        for (let index = 0; index < Austrudeln; index++) {
+            Intervall = Intervall / 2
+            if (Intervall == 1) {
+                Augen = randint(MinimumAugen, 6)
+                basic.setLedColor(0x00ff00)
+            } else {
+                Augen = randint(1, 6)
+            }
+            ZeigeAugen(Augen)
+            basic.pause(Intervall)
+        }
+    }
+})
