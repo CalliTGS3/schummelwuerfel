bf00bdcbc6d8c95b68ff9ff8f6230a3f2ff75327
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-12-30 15:07:03 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,89 @@
+function ZeigeAugen (AnzahlAugen: number) {
+    if (AnzahlAugen == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (AnzahlAugen == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (AnzahlAugen == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (AnzahlAugen == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (AnzahlAugen == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    if (MinimumAugen == 1) {
+        MinimumAugen = 3
+        basic.showIcon(IconNames.Happy)
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        MinimumAugen = 1
+        basic.showIcon(IconNames.Sad)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(100)
+    basic.clearScreen()
+    Intervall = 2 ** Austrudeln
+    for (let index = 0; index < Austrudeln; index++) {
+        Intervall = Intervall / 2
+        if (Intervall == 1) {
+            Augen = randint(MinimumAugen, 6)
+            basic.setLedColor(0x00ff00)
+            basic.pause(100)
+            basic.turnRgbLedOff()
+        } else {
+            Augen = randint(1, 6)
+        }
+        ZeigeAugen(Augen)
+        basic.pause(Intervall)
+    }
+})
+let Augen = 0
+let Intervall = 0
+let MinimumAugen = 0
+let Austrudeln = 0
+Austrudeln = 10
+MinimumAugen = 1
